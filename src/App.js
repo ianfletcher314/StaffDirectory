99360e4c3a2f10cae9895528aad66bf2bc59c47e
Ian Fletcher <ianfletcher314@gmail.com>
2021-06-15 15:57:07 -0500

    seach and table now connected to app.js. Next steps are 1) getting the api call set up 2) making the table load the information from the userDb 3) figuring out how to use materialize to make the site look better

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
-import Header from "./Components/Header/Header";
+import Header from "./Components/Header/Header"
+import Search from "./Components/Search/Search";
+import Table from "./Components/Table/Table";
+
 
 function App() {
-  return <Header />;
+  return <div>
+    <Header />,
+    <Search />,
+    <Table />;
+  </div>
+
 }
 
 export default App;
