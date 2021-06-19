63dd5a5a7df131dde03552a8bee6388a7e5560a4
Ian Fletcher <ianfletcher314@gmail.com>
2021-06-19 17:27:17 -0500

    redeploy

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Header() {
+  return (
+    <nav>
+    <div className="nav-wrapper purple accent-5">
+      <a href="/" className="brand-logo right">Company Directory</a>
+    </div>
+  </nav>
+  );
+}
+
+export default Header;
